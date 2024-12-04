4276bbf484489edd84d5d084b257b24a2d4ae6e3
marie-adi <marie.adim@gmail.com>
2024-12-04 11:38:59 +0100

    validations and others

--- src/pages/RegisterForm.jsx
+++ src/pages/RegisterForm.jsx
@@ -13,6 +13,39 @@ const RegisterForm = () => {
     const setRole = useStore((state) => state.setRole);
     const setUserName = useStore((state) => state.setUsername);
     const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    
+        if (!username || !password) {
+          setError('Por favor, completa todos los campos.');
+          return;
+        }
+    
+        try {
+          const response = await axios.post('http://localhost:5000/api/auth/login', { 
+            username, 
+            password 
+          });
+    
+          const { token, role, name } = response.data;
+    
+          if (token) {
+            // Almacenar datos en estado global
+            setToken(token);
+            setRole(role);
+            setUserName(name);
+    
+            // Redirigir según el rol
+            navigate(role === 'admin' ? '/admin-dashboard' : '/dashboard');
+          } else {
+            setError('Nombre de usuario o contraseña incorrectos.');
+          }
+        } catch (error) {
+          console.error('Error de autenticación:', error);
+          setError('No se recibió respuesta del servidor.');
+        }
+      };
 };
 
 export default RegisterForm;
