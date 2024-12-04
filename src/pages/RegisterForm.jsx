4688d16f4de16b50570a6e66753e3d68d795c323
marie-adi <marie.adim@gmail.com>
2024-12-04 13:55:54 +0100

    add input confirm password

--- src/pages/RegisterForm.jsx
+++ src/pages/RegisterForm.jsx
@@ -96,6 +96,24 @@ const RegisterForm = () => {
             />
           </div>
 
+          <div>
+            <label 
+              htmlFor="confirm-password" 
+              className="block text-red-500 font-medium mb-2"
+            >
+              Confirmar Contraseña:
+            </label>
+            <input
+              type="password"
+              id="confirm-password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              className="w-full border-2 border-gray-300 rounded-md p-2 focus:outline-none focus:ring focus:ring-red-200"
+              placeholder="Repite la contraseña"
+              required
+            />
+          </div>
+
         </form>
       </div>
     </div>
