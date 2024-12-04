f970b4a843976475482d0cab2201e9e193c90502
marie-adi <marie.adim@gmail.com>
2024-12-04 12:46:29 +0100

    starting to layout registration page

--- src/pages/RegisterForm.jsx
+++ src/pages/RegisterForm.jsx
@@ -31,6 +31,18 @@ const RegisterForm = () => {
           setError('Registro exitoso. Redirigiendo...');
         };
      
+    return (
+    <div 
+      className="flex justify-center items-center h-screen bg-cover bg-center" 
+      style={{ backgroundImage: 'url("../src/assets/Onda inicio.png")' }}>
+        
+      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+        <h2 className="text-2xl font-bold text-center text-gray-800 mb-4">
+          Registro de Usuario
+        </h2>
+      </div>
+    </div>
+  );
 };
 
 export default RegisterForm;
