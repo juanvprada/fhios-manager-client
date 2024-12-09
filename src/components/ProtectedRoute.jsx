aa633192d81da6d7d3cd7b8dd807c567d674ba69
Monica <monicut3@gmail.com>
2024-12-09 21:48:53 +0100

    Feat: Add proptypes

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,9 +1,10 @@
-// ProtectedRoute.jsx
+import React from "react"; // Asegúrate de importar React
+import PropTypes from "prop-types"; // Importa PropTypes para validar las props// ProtectedRoute.jsx
 import { useMemo } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import useStore from '../store/store';
 
-const ProtectedRoute = ({ children, adminOnly = false }) => {
+const ProtectedRoute = ({ children, adminOnly }) => {
   // Usar useMemo para evitar recálculos innecesarios
   const authState = useMemo(() => {
     return useStore.getState();
@@ -28,6 +29,17 @@ const ProtectedRoute = ({ children, adminOnly = false }) => {
 
   // Renderizar children si existen, sino usar Outlet para rutas anidadas
   return children || <Outlet />;
+};
+
+// Validaciones de las props
+ProtectedRoute.propTypes = {
+  children: PropTypes.node, // Validación para children
+  adminOnly: PropTypes.bool, // Validación para adminOnly
+};
+
+// Valores por defecto para las props
+ProtectedRoute.defaultProps = {
+  adminOnly: false,
 };
 
 export default React.memo(ProtectedRoute);
